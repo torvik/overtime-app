ae87321400c9fbb086a0e630c742d4514db2e6f2
torvik <pless.victor@gmail.com>
2016-09-07 16:02:12 +0000

    Created index action for posts

--- spec/features/static_spec.rb
+++ spec/features/static_spec.rb
@@ -1,10 +1,16 @@
 require 'rails_helper'
 
 describe 'navigate' do
-  describe 'homepage' do
-    it 'can be reached successfully' do
-      visit root_path
+  describe 'index' do
+    it 'cant be reached successfully' do
+      visit posts_path
       expect(page.status_code).to eq(200)
     end
+
+     it 'has a title of Post' do
+      visit posts_path
+      expect(page).to have_content(/Post/)
+    end
+
   end
 end
