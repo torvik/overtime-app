62583a30d97f1d7bfdb2cc5b7842620ac7337134
torvik <pless.victor@gmail.com>
2016-09-07 17:03:10 +0000

    posts adn refactored

---
+++ spec/features/post_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+describe 'navigate' do
+  describe 'index' do
+    it 'cant be reached successfully' do
+      visit posts_path
+      expect(page.status_code).to eq(200)
+    end
+
+     it 'has a title of Post' do
+      visit posts_path
+      expect(page).to have_content(/Post/)
+    end
+
+  end
+
+  describe "creation" do
+    before do
+      visit new_post_path
+    end
+
+      it 'has a new form that can be reached' do
+        expect(page.status_code).to eq(200)
+      end
+
+      it 'can be created from new form page' do
+          fill_in 'post[date]', with: Date.today
+          fill_in 'post[rationale]', with: "Some rationale"
+          click_on "Save"
+          expect(page).to have_content("Some rationale")
+      end
+  end
+end
