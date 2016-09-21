0c170ede027df4abe5d9e87b3aef50fb9362e26f
torvik <pless.victor@gmail.com>
2016-09-21 13:38:38 +0000

    Implemented user connect

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,6 +9,7 @@ before_action :set_post, only: [:show]
 
   def create
     @post = Post.new(post_params)
+    @post.user_id = current_user.id 
 
    if @post.save
       redirect_to @post, notice: "Your post was created"
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ActiveRecord::Base
 
     validates_presence_of :date, :rationale
+    belongs_to :user
 end
